1d894aa808dbaf0b3f74073e5001bca006c80787
Yolanda Arnaspen <210401069@student.umri.ac.id>
2023-10-11 21:27:46 +0700

    first commit

--- Tes_Macro/app/build.gradle.kts
+++ Tes_Macro/app/build.gradle.kts
@@ -1,17 +1,16 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-parcelize")
 }
 
 android {
-    namespace = "com.example.fundamental1"
-    compileSdk = 34
+    namespace = "com.example.tes_macro"
+    compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.example.fundamental1"
+        applicationId = "com.example.tes_macro"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -38,8 +37,6 @@ android {
 
 dependencies {
 
-    implementation ("androidx.appcompat:appcompat:1.3.1")
-    implementation ("androidx.cardview:cardview:1.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
