e4d3012e2b31464f13698971945da34bb812d06b
Yolanda Arnaspen <210401069@student.umri.ac.id>
2023-10-12 01:19:29 +0700

    add Tes_Macro

--- Tes_Macro/app/build.gradle.kts
+++ Tes_Macro/app/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
 }
 
 android {
     namespace = "com.example.tes_macro"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.tes_macro"
+        applicationId = "com.tes_macro"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -37,6 +38,8 @@ android {
 
 dependencies {
 
+    implementation ("androidx.appcompat:appcompat:1.3.1")
+    implementation ("androidx.cardview:cardview:1.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
---
+++ Tes_Macro/app/src/main/java/com/example/tes_macro/YolaArtikel.kt
@@ -0,0 +1,85 @@
+package com.example.tes_macro
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+import android.widget.ImageView
+import android.widget.TextView
+
+class YolaArtikel : AppCompatActivity() {
+
+    private lateinit var textViewForYou: TextView
+    private lateinit var textViewRecommendation: TextView
+    private lateinit var textViewTranding: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_yola_artikel)
+
+        textViewForYou = findViewById(R.id.textViewForYou)
+        textViewRecommendation = findViewById(R.id.textViewRecommendation)
+        textViewTranding = findViewById(R.id.textViewTranding)
+
+        textViewForYou.setOnClickListener {
+            updateUnderline(textViewForYou)
+            resetUnderline(textViewRecommendation, textViewTranding)
+        }
+
+        textViewRecommendation.setOnClickListener {
+            updateUnderline(textViewRecommendation)
+            resetUnderline(textViewForYou, textViewTranding)
+        }
+
+        textViewTranding.setOnClickListener {
+            updateUnderline(textViewTranding)
+            resetUnderline(textViewForYou, textViewRecommendation)
+        }
+
+        // Set data for Artikel
+        val artikelList = listOf(
+            ArtikelData(R.drawable.artike1, "Pentingnya Pendidikan Bagi Masa Depan", "Oleh : Dispendik Mojokerto", "15 Agustus 2023"),
+            ArtikelData(R.drawable.artikel2, "8 Cara Membuat Katalog Online untuk Tingkat Bisnis", "Oleh : Redaksi Jagoan Hosting", "15 Agustus 2023"),
+            ArtikelData(R.drawable.artikel4, "Para Penerima Beasiswa Amanah Bangun Desa Telah Memasuki Tahap Implementasi Proyek", "Oleh : Kompasiana", "15 Agustus 2023"),
+            ArtikelData(R.drawable.artikel5, "LazisMu UMS Berikan Beasiswa Hingga Lulus", "Oleh : Dispendik Mojokerto", "15 Agustus 2023"),
+            ArtikelData(R.drawable.artikel6, "Pengumuman Pendaftaran Pewawancara Seleksi Beasiswa Indonesia Bangkit 2023", "Oleh : Dispendik Mojokerto", "15 Agustus 2023"),
+            ArtikelData(R.drawable.artikel7, "Pengumuman Pendaftaran Pewawancara Seleksi Beasiswa Indonesia Bangkit 2023", "Oleh : Dispendik Mojokerto", "15 Agustus 2023")
+        )
+
+        setArtikelData(artikelList)
+    }
+
+    private fun updateUnderline(textView: TextView) {
+        val content = SpannableString(textView.text)
+        content.setSpan(UnderlineSpan(), 0, content.length, 0)
+        textView.text = content
+    }
+
+    private fun resetUnderline(vararg textViews: TextView) {
+        textViews.forEach {
+            it.text = removeUnderline(it)
+        }
+    }
+
+    private fun removeUnderline(textView: TextView): CharSequence {
+        val content = SpannableString(textView.text)
+        content.removeSpan(UnderlineSpan())
+        return content
+    }
+
+    private fun setArtikelData(artikelList: List<ArtikelData>) {
+        for ((index, artikelData) in artikelList.withIndex()) {
+            val imageView: ImageView = findViewById(resources.getIdentifier("imageView${index + 1}", "id", packageName))
+            val textViewTitle = findViewById<TextView>(resources.getIdentifier("textViewTitle${index + 1}", "id", packageName))
+            val textViewPublisher = findViewById<TextView>(resources.getIdentifier("textViewPublisher${index + 1}", "id", packageName))
+            val textViewDate = findViewById<TextView>(resources.getIdentifier("textViewDate${index + 1}", "id", packageName))
+            val imageViewShare: ImageView = findViewById(resources.getIdentifier("imageViewShare${index + 1}", "id", packageName))
+
+            imageView.setImageResource(artikelData.imageRes)
+            textViewTitle.text = artikelData.title
+            textViewPublisher.text = artikelData.publisher
+            textViewDate.text = artikelData.date
+        }
+    }
+}
+
+data class ArtikelData(val imageRes: Int, val title: String, val publisher: String, val date: String)
